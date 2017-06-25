ee9d9fe2fdb72f43b4c2a79c1c9d46e56c5712be
Bruno Ribeiro <brunz36@gmail.com>
2017-06-25 12:54:32 -0400

    Login/Logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,10 @@ class User < ApplicationRecord
 
   validates :password,  presence: true,
                         length: { in: 6..20 }
+
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
