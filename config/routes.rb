9632d8a6ceeabf56aed8c16dcc2e85b05e5d0071
Bruno Ribeiro <brunz36@gmail.com>
2017-06-24 22:35:21 -0400

    removed scaffolds

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  resources :microposts
-  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
