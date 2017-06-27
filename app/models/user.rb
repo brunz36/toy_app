eb305853c4611c17e35c4c9910eef469d0fd540a
Bruno Ribeiro <brunz36@gmail.com>
2017-06-26 22:45:58 -0400

    Authentication Method

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,9 +32,9 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    digest.nil? ? false : BCrypt::Password.new(digest).is_password?(token)
   end
 
   def forget
